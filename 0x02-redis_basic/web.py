ac9bb6f6ab198a8c0439cf0b455ee8a30bc829c5
xaranx <arrif.bouchaib03@gmail.com>
2024-03-27 18:02:25 -0700

    adv task fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,21 @@
 #!/usr/bin/env python3
-"""module for request caching.
 """
-import redis
-import requests
-from functools import wraps
+Implementing an expiring web cache and tracker
+"""
+
 from typing import Callable
+from functools import wraps
 
 
-redis_store = redis.Redis()
-
-
-def cache_req(method: Callable) -> Callable:
-    """Caches the output of fetched data."""
-
+def count_requests(method: Callable) -> Callable:
+    """
+    Track how many times a url has been accessed
+    """
     @wraps(method)
-    def inner(url) -> str:
-        """The wrapper function for caching the output."""
-        count_key = "count:{}".format(url)
-        result_key = "result:{}".format(url)
-        redis_store.expire(count_key, 10)
-        result = redis_store.get(result_key)
-        if result:
-            return result.decode("utf-8")
-        result = method(url)
-        redis_store.set(result_key, result, ex=10)
-        redis_store.incr(count_key)
-        return result
-
-    return inner
-
-
-@cache_req
-def get_page(url: str) -> str:
-    """Returns the content of a URL after caching the response."""
-    return requests.get(url).text
+    def wrapper(self, url: str) -> str:
+        """
+        Wrapper function
+        """
+        self._redis.incr(f"count:{url}")
+        return method(self, url)
+    return wrapper
